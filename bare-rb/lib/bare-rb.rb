7175f39cf0b0f7aa56485f66f1759c34d5684e1b
n8ta <n8@u.northwestern.edu>
2020-10-14 20:44:00 -0500

    implement uint, todo: proper parser that returns unused binary data

--- bare-rb/lib/bare-rb.rb
+++ bare-rb/lib/bare-rb.rb
@@ -18,8 +18,24 @@ class Bare
       return [msg].pack("l<")
     when Bare::DataTypes::I64
       return [msg].pack("q<")
+    when Bare::DataTypes::Bool
+      return msg ? "\xFF\xFF".b : "\x00\x00".b
+    when Bare::DataTypes::Uint
+      bytes = "".b
+      get_next_7_bits_as_byte(msg, 128) do |byte|
+        bytes << byte
+      end
+      (bytes.size - 1).downto(0) do |i|
+        if bytes.bytes[i] == 128
+          bytes = bytes.chop
+        else
+          break
+        end
+      end
+      bytes[bytes.size - 1] = [bytes.bytes[bytes.size - 1] & 127].pack('C')[0]
+      return bytes
     else
-      raise("Bad schema")
+      raise("Unknown type: #{schema.inspect}. make sure you call .new on Bare::DataType::XYZ!")
     end
   end
 
@@ -42,31 +58,70 @@ class Bare
       return msg.unpack('l<')[0]
     when Bare::DataTypes::I64
       return msg.unpack('q<')[0]
+    when Bare::DataTypes::Bool
+      return msg == "\x00\x00" ? false : true
+    when Bare::DataTypes::Uint
+      ints = msg.unpack("C*")
+      relevantInts = []
+      i = 0
+      while ints[i] & 0b10000000 == 128
+        relevantInts << ints[i] % 128
+        i += 1
+      end
+      relevantInts << ints[i]
 
+      sum = 0
+      relevantInts.each_with_index do |int, idx|
+        sum += int << (idx * 7)
+      end
+      return sum
     else
-      raise("Data doesn't match schema")
+      raise("Unknown type found in schema: #{schema}")
     end
   end
-
 
   class DataTypes
+    class Uint
+    end
+
     class U8
     end
+
     class U16
     end
+
     class U32
     end
+
     class U64
     end
+
     class I8
     end
+
     class I16
     end
+
     class I32
     end
+
     class I64
     end
+
+    class Bool
+    end
   end
+end
 
-
+def get_next_7_bits_as_byte(integer, base = 128)
+  # Base is the initial value of the byte before
+  # before |'ing it with 7bits from the integer
+  groups_of_7 = (integer.size*8) / 7 + (integer.size % 7 == 0 ? 0 : 1)
+  0.upto(groups_of_7 - 1) do |group_number|
+    byte = base
+    0.upto(7).each do |bit_number|
+      byte = byte | (integer[group_number * 7 + bit_number] << bit_number)
+    end
+    yield(byte)
+  end
 end
--- tests/basic.rb
+++ tests/basic.rb
@@ -1,67 +1,25 @@
 require '../bare-rb/lib/bare-rb'
 
-samples = [
+encode_decode_tests = [
     # input, expected output, schema
-    [1, "\x01".b, Bare::DataTypes::U8.new],
-    [3, "\x03".b, Bare::DataTypes::U8.new],
-    [255, "\xFF".b, Bare::DataTypes::U8.new],
-
-    [1, "\x01\x00".b, Bare::DataTypes::U16.new],
-    [3, "\x03\x00".b, Bare::DataTypes::U16.new],
-    [256, "\x00\x01".b, Bare::DataTypes::U16.new],
-    [(2**16)-1, "\xFF\xFF".b, Bare::DataTypes::U16.new],
-
-    [1, "\x01\x00\x00\x00".b, Bare::DataTypes::U32.new],
-    [3, "\x03\x00\x00\x00".b, Bare::DataTypes::U32.new],
-    [256+3, "\x03\x01\x00\x00".b, Bare::DataTypes::U32.new],
-    [4278190080, "\x00\x00\x00\xFF".b, Bare::DataTypes::U32.new],
-    [(2**32)-1, "\xFF\xFF\xFF\xFF".b, Bare::DataTypes::U32.new],
-
-    [1, "\x01\x00\x00\x00\x00\x00\x00\x00".b, Bare::DataTypes::U64.new],
-    [3, "\x03\x00\x00\x00\x00\x00\x00\x00".b, Bare::DataTypes::U64.new],
-    [256+3, "\x03\x01\x00\x00\x00\x00\x00\x00".b, Bare::DataTypes::U64.new],
-    [(2**64)-1, "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF".b, Bare::DataTypes::U64.new],
-    [(2**64)-2, "\xFE\xFF\xFF\xFF\xFF\xFF\xFF\xFF".b, Bare::DataTypes::U64.new],
-
-    [1, "\x01".b, Bare::DataTypes::I8.new],
-    [-1, "\xFF".b, Bare::DataTypes::I8.new],
-    [3, "\x03".b, Bare::DataTypes::I8.new],
-    [-3, "\xFD".b, Bare::DataTypes::I8.new],
-    [-2**7, "\x80".b, Bare::DataTypes::I8.new],
-    [(2**7)-1, "\x7F".b, Bare::DataTypes::I8.new],
-
-    [1, "\x01\x00".b, Bare::DataTypes::I16.new],
-    [-1, "\xFF\xFF".b, Bare::DataTypes::I16.new],
-    [3, "\x03\x00".b, Bare::DataTypes::I16.new],
-    [-3, "\xFD\xFF".b, Bare::DataTypes::I16.new],
-    [-500, "\x0C\xFE".b, Bare::DataTypes::I16.new],
-    [500, "\xF4\x01".b, Bare::DataTypes::I16.new],
-    [-2**15, "\x00\x80".b, Bare::DataTypes::I16.new],
-    [(2**15)-1, "\xFF\x7F".b, Bare::DataTypes::I16.new],
-
-    [1, "\x01\x00\x00\x00".b, Bare::DataTypes::I32.new],
-    [-1, "\xFF\xFF\xFF\xFF".b, Bare::DataTypes::I32.new],
-    [3, "\x03\x00\x00\x00".b, Bare::DataTypes::I32.new],
-    [-3, "\xFD\xFF\xFF\xFF".b, Bare::DataTypes::I32.new],
-    [-500, "\x0C\xFE\xFF\xFF".b, Bare::DataTypes::I32.new],
-    [-5000000, "\xC0\xB4\xB3\xFF".b, Bare::DataTypes::I32.new],
-    [500, "\xF4\x01\x00\x00".b, Bare::DataTypes::I32.new],
-    [-2**31, "\x00\x00\x00\x80".b, Bare::DataTypes::I32.new],
-    [(2**31)-1, "\xFF\xFF\xFF\x7F".b, Bare::DataTypes::I32.new],
-
-    [1, "\x01\x00\x00\x00\x00\x00\x00\x00".b, Bare::DataTypes::I64.new],
-    [-1, "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF".b, Bare::DataTypes::I64.new],
-    [3, "\x03\x00\x00\x00\x00\x00\x00\x00".b, Bare::DataTypes::I64.new],
-    [-3, "\xFD\xFF\xFF\xFF\xFF\xFF\xFF\xFF".b, Bare::DataTypes::I64.new],
-    [-500, "\x0C\xFE\xFF\xFF\xFF\xFF\xFF\xFF".b, Bare::DataTypes::I64.new],
-    [-5000000, "\xC0\xB4\xB3\xFF\xFF\xFF\xFF\xFF".b, Bare::DataTypes::I64.new],
-    [500, "\xF4\x01\x00\x00\x00\x00\x00\x00".b, Bare::DataTypes::I64.new],
-    [-2**63, "\x00\x00\x00\x00\x00\x00\x00\x80".b, Bare::DataTypes::I64.new],
-    [(2**63)-1, "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x7F".b, Bare::DataTypes::I64.new],
-    [-50000000000000000, "\x00\x00\x3B\xD1\x43\x5D\x4E\xFF".b, Bare::DataTypes::I64.new ]
+    [1, "\x01".b, Bare::DataTypes::Uint.new],
+    [127, "\x7F".b, Bare::DataTypes::Uint.new],
+    [1, "\x01".b, Bare::DataTypes::Uint.new],
+    [8, "\x08".b, Bare::DataTypes::Uint.new],
+    [128, "\x80\x01".b, Bare::DataTypes::Uint.new],
+    [129, "\x81\x01".b, Bare::DataTypes::Uint.new],
+    [22369, "\xE1\xAE\x01".b, Bare::DataTypes::Uint.new],
+    [16383,"\xFF\x7F".b, Bare::DataTypes::Uint.new],
+    [16382,"\xFE\x7F".b, Bare::DataTypes::Uint.new],
 ]
 
-samples.each_with_index do  |sample,i|
+decode_tests = [
+    ["\x05\x00".b, true, Bare::DataTypes::Bool.new],
+    ["\x00\x70".b, true, Bare::DataTypes::Bool.new],
+    ["\x00\x00".b, false, Bare::DataTypes::Bool.new],
+]
+
+encode_decode_tests.each_with_index do |sample, i|
   output = Bare.encode(sample[0], sample[2])
   if output != sample[1]
     raise "\nTest #{sample[0]} - ##{i.to_s} (#{sample[2].class.name}) failed\nreal output: #{output.inspect} \ndoesn't match expected output: #{sample[1].inspect}"
@@ -69,5 +27,12 @@ samples.each_with_index do  |sample,i|
   decoded = Bare.decode(output, sample[2])
   if decoded != sample[0]
     raise "\nTest #{sample[0]} - ##{i.to_s} (#{sample[2].class.name}) failed\n decode #{decoded.inspect} \ndoesn't match input #{sample[0].inspect}"
+  end
+end
+
+decode_tests.each_with_index do |test, i|
+  decoded = Bare.decode(test[0], test[2])
+  if decoded != test[1]
+    raise "\nDecode Test #{test[0]} - ##{i.to_s} (#{test[2].class.name}) failed\n decoding #{test[0].inspect} isn't equal to  #{test[1].inspect}\n instead got #{decoded.inspect}"
   end
 end
