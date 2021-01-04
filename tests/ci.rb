ad87e0bf9db6982bde4bac1b8ec79c4391d55176
n8ta <n8@u.northwestern.edu>
2021-01-04 03:56:57 -0600

    ci

--- tests/ci.rb
+++ tests/ci.rb
@@ -1,7 +1,9 @@
 require 'coveralls'
 require 'simplecov'
 
+puts "Starting code coverage"
 Coveralls.wear!
 SimpleCov.command_name 'Custom BARE Test Script'
+puts "Calling tests.rb script"
 
 require_relative './tests.rb'
