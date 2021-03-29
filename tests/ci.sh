f9fed6afebe6ad77ddeb71621cf0949ecec648a2
n8ta <n8@u.northwestern.edu>
2021-03-29 03:11:19 -0500

    get pip python 3.7

--- tests/ci.sh
+++ tests/ci.sh
@@ -5,5 +5,5 @@ gem install coveralls
 gem install simplecov
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
-pip install setuptools
+pip3 install setuptools
 ruby tests/ci.rb
