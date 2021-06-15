de7782aa738910952785cdd0f1e7630549646bc0
n8ta <n8@u.northwestern.edu>
2021-06-14 21:25:00 -0500

    chdir

--- ci.rb
+++ ci.rb
@@ -11,6 +11,6 @@ require_relative 'tests/tests.rb'
 
 puts "Calling bare-py to test interoperability"
 
-compat = File.join(__dir__, "compat")
-system("#{compat} #{__dir__}") || exit(-1)
+compat = File.join(__dir__,  "tests", "compat")
+system("#{compat} #{__dir__}", chdir: 'tests') || exit(-1)
 
